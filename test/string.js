5c8683cac5ed41762403c69396f354690e7459e4
Brandon Valosek <bvalosek@gmail.com>
2014-03-18 14:30:15 -0500

    IterableString stuff

---
+++ test/string.js
@@ -0,0 +1,31 @@
+var test           = require('tape');
+var util           = require('./util.js');
+var IterableString = require('../lib/IterableString.js');
+
+test('Iterator inteface', function(t) {
+  t.plan(1);
+  var s = new IterableString('hello');
+  t.ok(util.implementsIterable(s));
+});
+
+test('Yield string results in order', function(t) {
+  t.plan(1);
+  var s = new IterableString('hello');
+  t.ok(util.iterableEqualArray(s, ['h','e','l','l','o']));
+});
+
+test('Empty string', function(t) {
+  t.plan(1);
+  var s = new IterableString('');
+  t.ok(util.iterableEqualArray(s, []));
+});
+
+test('Parameter validation', function(t) {
+  t.plan(6);
+  t.throws(function() { new IterableString(['asdf']); }, TypeError);
+  t.throws(function() { new IterableString(null); }, TypeError);
+  t.throws(function() { new IterableString(undefined); }, TypeError);
+  t.throws(function() { new IterableString(); }, TypeError);
+  t.throws(function() { new IterableString(4); }, TypeError);
+  t.throws(function() { new IterableString(true); }, TypeError);
+});
