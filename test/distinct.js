880cf8a9dd9c7207b86619f76f30ad1f4c26ee9b
Brandon Valosek <bvalosek@gmail.com>
2014-03-20 21:32:47 -0500

    Distict and reduce

---
+++ test/distinct.js
@@ -0,0 +1,26 @@
+var test             = require('tape');
+var util             = require('./util.js');
+var DistinctIterable = require('../lib/DistinctIterable.js');
+
+test('Default select', function(t) {
+  t.plan(1);
+  var d = new DistinctIterable(util.fromArray([1,2,2,3,1]));
+  t.ok(util.iterableEqualArray(d, [1,2,3]));
+});
+
+test('Selector', function(t) {
+  t.plan(1);
+  var d = new DistinctIterable(
+    util.fromArray([1.1, 1.2, 1.3,2.1,2.2]),
+    function(x) { return 0|x; }
+  );
+
+  t.ok(util.iterableEqualArray(d, [1.1,2.1]));
+});
+
+test('Param validation', function(t) {
+  t.plan(2);
+  t.throws(function() { new DistinctIterable(util.empty, 'garbage'); });
+  t.throws(function() { new DistinctIterable('gabage', util.identity); });
+
+});
---
+++ test/reduce.js
@@ -0,0 +1,28 @@
+var test     = require('tape');
+var util     = require('./util.js');
+var Sequence = require('../lib/Sequence.js');
+
+test('Basic reduce with no seed', function(t) {
+  t.plan(1);
+  var s = Sequence.from([0, 1, 4, 5]);
+  var v = s.reduce(function(acc, x) { return acc + x * 2; });
+  t.strictEqual(v, 20);
+});
+
+test('Basic reduce with seed', function(t) {
+  t.plan(1);
+  var s = Sequence.from([1, 4, 5]);
+  var v = s.reduce(function(acc, x) { return acc + x * 2; }, 0);
+  t.strictEqual(v, 20);
+});
+
+test('Empty sequence', function(t) {
+  t.plan(1);
+  t.strictEqual(Sequence.empty.reduce(function() { }), undefined);
+});
+
+test('Param validation', function(t) {
+  t.plan(2);
+  t.throws(function() { Sequence.empty.reduce(); }, TypeError);
+  t.throws(function() { Sequence.empty.reduce('garbage'); }, TypeError);
+});
